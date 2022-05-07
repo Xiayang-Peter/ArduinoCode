1110b0c78460742caf88ce6ccc90750bfccec563
Xiayang-Peter <xiayang123wert@outlook.com>
2022-05-07 15:03:10 +1200

    2022/5/7

--- BLE-Phone/BLE-Phone.ino
+++ BLE-Phone/BLE-Phone.ino
@@ -1,26 +1,46 @@
-// Arduino code to send messages from smartphone to Arduino
-#include <SoftwareSerial.h>
-
+#include <SoftwareSerial.h> //BLUETOOTH
 SoftwareSerial ble_device(3,4);
-
-String str_ii = "";
+String str_ii = "";  //set bleinput value
 int ii_0 = 0;
+String DisplayContext = "";
+
+#include <MD_Parola.h>     //DISPLAY
+#include <MD_MAX72XX.h>
+#include <SPI.h>
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW  //define hardware spi pins for display
+#define MAX_DEVICES 4
+#define CS_PIN 10
+
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES); //new configuret for display
+
+// #define DATA_PIN 11             //reserve for software spi pins setting
+// #define CLK_PIN 13
+// MD_Parola matrix = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+
 
 void setup() {  
-  ble_device.begin(9600);
+  Serial.begin(115200);
+  ble_device.begin(9600);                                         //BLE setups
   delay(100);
-
-  // Enter AT+ commands of interest here (BLE Address, UUIDs, Power settings)
-  ble_cmd("AT+NAMEBLExAR","Device Name: "); // printout device name
+  ble_cmd("AT+NAMEtest1","Device Name: "); // change and printout device name
+  /*
   ble_cmd("AT+LADDR","Address: "); // printout BLE address
   ble_cmd("AT+CHAR","Char UUID: "); // printout character UUID
   ble_cmd("AT+VERSION","Version: "); // module version
   ble_cmd("AT+RESET","Reset: "); // reset module
   str_ii = "";
   delay(500);
+  */
+
+                                     
+  myDisplay.begin();                                           //DISPALY setups
+  // Set the brightness of the display ( from 0-15):
+  myDisplay.setIntensity(0);
+  myDisplay.displayClear();
 }
 
-
+//running ble_cmd
 String ble_cmd(String cmd_str,String desc_str){
   str_ii = "";
   unsigned long t1 = millis();
@@ -42,17 +62,26 @@ String ble_cmd(String cmd_str,String desc_str){
 
 
 void loop() {
-  while (ble_device.available()){
+  while (ble_device.available()){                              //BLE loop
     char in_char = ble_device.read();
     if (int(in_char)!=-1 and int(in_char)!=42){
       str_ii+=in_char;
     }
     if (in_char=='\n'){
       delay(20);
-      String msg = "Msg: ";
+      String msg = "Msg received: ";
       msg+=str_ii;
       ble_device.print(msg);
+      //myDisplay.setTextAlignment(PA_CENTER);                     //DISPLAY loop 
+      //myDisplay.print(str_ii);
+      //Serial.println(str_ii);
+      //delay(2000);
       str_ii = "";
     }
   }
+  Serial.println(DisplayContext);
+  myDisplay.setTextAlignment(PA_CENTER);                 
+  myDisplay.print(DisplayContext);
+  delay(2000);
+  DisplayContext = "";
 }
--- LED_MARTIX/LED_MARTIX.ino
+++ LED_MARTIX/LED_MARTIX.ino
@@ -7,37 +7,31 @@
 #define MAX_DEVICES 4
 #define CS_PIN 10
 
-// Create a new instance of the MD_MAX72XX class:
+// config hardware spi pins and display type:
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
-// For software SPI you also need to specify the DATA_PIN and the CLK_PIN connections:
+// reserve code for software spi config:
 // #define DATA_PIN 11
 // #define CLK_PIN 13
-
-// Create a new instance of the MD_MAX72XX class:
 // MD_Parola matrix = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
 
 void setup() {
-  // Intialize the object:
   myDisplay.begin();
-  // Set the intensity (brightness) of the display (0-15):
-  myDisplay.setIntensity(0);
-  // Clear the display:
   myDisplay.displayClear();
+  // Set the brightness of the display (0-15):
+  myDisplay.setIntensity(0);
+  myDisplay.displayText("Hello, Visual Studio code !", PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 }
 
 void loop() {
-  myDisplay.setTextAlignment(PA_CENTER);
-  myDisplay.print("Hello");
-  delay(2000);
-  myDisplay.setTextAlignment(PA_CENTER);
-  myDisplay.print("VScode");
-  delay(2000);
-
- // myDisplay.setTextAlignment(PA_CENTER);
-//  myDisplay.setInvert(true);
-//  myDisplay.print("Invert");
-//  delay(2000);
-
+  //myDisplay.setTextAlignment(PA_CENTER);
+  //myDisplay.print("Hello");
+  //delay(2000);
+  //myDisplay.setTextAlignment(PA_CENTER);
+  //myDisplay.print("Visual studio code");
+  //delay(2000);
+  if (myDisplay.displayAnimate()) {
+  myDisplay.displayReset();
+  }
 }
