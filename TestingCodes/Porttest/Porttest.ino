964ccd8dcebac2dda76e02914fc4d285a117ad6c
Xiayang-Peter <xiayang123wert@outlook.com>
2022-05-30 12:06:26 +1200

    5/30 LED test

---
+++ TestingCodes/Porttest/Porttest.ino
@@ -0,0 +1,26 @@
+
+#define led2 2;
+#define led3 3;
+#define led4 4;
+#define led5 5;
+#define led6 6;
+#define led7 7;
+#define led8 8;
+#define led9 9;
+#define led10 10;
+#define led11 11;
+#define led12 12;
+
+
+
+
+
+void setup(){
+    pinMode(led4, INPUT);
+    pinMode(led3, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(led3, HIGH); // Turn the LED on
+
+}
